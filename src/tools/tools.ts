208a9921666533dcb7b835fc4edd1d0890b88349
Jonnytoshen <jonnytoshen@gmail.com>
2026-05-26 13:40:17 +0800

    feat(tools): 注册 fetch_url 和 start_preview 工具

--- src/tools/tools.ts
+++ src/tools/tools.ts
@@ -2,10 +2,12 @@ import type { ToolDefinition } from './tool-registry';
 import { BashTool } from './BashTool';
 import { CalculatorTool } from './CalculatorTool';
 import { EditFileTool } from './EditFileTool';
+import { FetchUrlTool } from './FetchUrlTool';
 import { GlobTool } from './GlobTool';
 import { GrepTool } from './GrepTool';
 import { ListDirectoryTool } from './ListDirectoryTool';
 import { ReadFileTool } from './ReadFileTool';
+import { StartPreviewTool } from './StartPreviewTool';
 import { WeatherTool } from './WeatherTool';
 import { WriteFileTool } from './WriteFileTool';
 
@@ -16,10 +18,12 @@ export const allTools: ToolDefinition[] = [
   BashTool,
   CalculatorTool,
   EditFileTool,
+  FetchUrlTool,
   GlobTool,
   GrepTool,
   ListDirectoryTool,
   ReadFileTool,
+  StartPreviewTool,
   WriteFileTool,
   WeatherTool,
 ];
